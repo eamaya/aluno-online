e66695d55fb86ab86e13aa0ea1c05320f2ed0bb7
Gustavo Xudré <gxudre@gmail.com>
2023-04-27 15:38:07 -0300

    fixed V0.1 Media

--- src/models/nota.js
+++ src/models/nota.js
@@ -1,22 +1,33 @@
 class Nota {
-    constructor(disciplina, a1, a2, a3) {
-        this.disciplina = disciplina;
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
-    }
+  constructor(disciplina, a1, a2, a3) {
+    this.disciplina = disciplina;
+    this.a1 = a1;
+    this.a2 = a2;
+    this.a3 = a3;
+  }
 
-    mediaFinal() {
-        return Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
-            0.4 * this.a3 + 0.6 * this.a2
-        );
-    }
+  mediaFinal() {
+    return Math.max(
+      0.4 * this.a1 + 0.6 * this.a2,
+      0.4 * this.a1 + 0.6 * this.a3,
+      0.4 * this.a3 + 0.6 * this.a2
+    );
+  }
 
-    mediaCA() {
-        return "SS";
+  mediaCA() {
+    const media = this.mediaFinal();
+    if (media >= 8 && media <= 10) {
+      return "SS";
+    } else if (media >= 6.6 && media < 8) {
+      return "MS";
+    } else if (media >= 5 && media < 6.6) {
+      return "MM";
+    } else if (media >= 3 && media < 5) {
+      return "MI";
+    } else {
+      return "II";
     }
+  }
 }
 
 module.exports = Nota;
