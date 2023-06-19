a16a41ae7731040a5717a25e618c823aacb942f0
Italo-Padre <italo.pa61@gmail.com>
2023-06-19 20:45:21 -0300

    19

--- src/models/nota.js
+++ src/models/nota.js
@@ -8,15 +8,21 @@ class Nota {
 
     mediaFinal() {
         return Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
+            0.4 * this.a1 + 0.6 * this.a2,
+            0.4 * this.a1 + 0.6 * this.a3,
             0.4 * this.a3 + 0.6 * this.a2
         );
     }
 
+   
     mediaCA() {
-        return "SS";
-    }
+        if (mediaFinal >= 9 && mediaFinal <= 10) return "SS"; 
+            else if (mediaFinal >= 7 && mediaFinal < 9) return "MS";
+         else if (mediaFinal >= 5 && mediaFinal < 7) return 'MM';
+         else if (mediaFinal >= 3 && mediaFinal < 5) return 'MM';
+         else if (mediaFinal >= 0 && mediaFinal < 3) return 'ii';
+         else return'SR'
+        }
 }
 
-module.exports = Nota;
+    module.exports = Nota;
