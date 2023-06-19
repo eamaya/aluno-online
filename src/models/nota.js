84db46627143d377bb4095ab36d22b3a1ddf36b6
Marcos <markvini055@gmail.com>
2023-06-19 17:58:27 -0300

    Update Test Unit nota

--- src/models/nota.js
+++ src/models/nota.js
@@ -12,12 +12,21 @@ function nota(disciplina, a1, a2, a3) {
 
     function mediaCA() {
         const media = mediaFinal();
-        if (media >= 5) {
-            return "Aprovado";
-        }  else {
-            return "Reprovado";
+        if (media >= 9.0) {
+            return 'SS'; // Superior
+        } else if (media >= 7.0) {
+            return 'MS'; // Médio Superior
+        } else if (media >= 5.0) {
+            return 'MM'; // Médio
+        } else if (media >= 3.0) {
+            return 'MI'; // Médio Inferior
+        } else if (media >= 0.1) {
+            return 'II'; // Inferior
+        } else {
+            return 'SR'; // Sem rendimento
         }
     }
+    
 
     return { disciplina, a1, a2, a3, mediaFinal, mediaCA };
 }
--- test/unit/specs/nota.test.js
+++ test/unit/specs/nota.test.js
@@ -1,35 +1,16 @@
-const Nota = require('../src/models/nota');
+const nota = require('../../src/models/nota');
 
-describe('Calculo da média final', () => {
-    test('a media é zero se não tem notas', () => {
-        let nota = new Nota(null, 0, 0, 0);
-        expect(nota.mediaFinal()).toEqual(0);
-        nota = new Nota(null, null, null, null)
-        expect(nota.mediaFinal()).toEqual(0);
-    });
-
-    test('a média é (0.4 * a1) + (0.6 * a2) se tem apenas a1 e a2', () => {
-        let nota = new Nota(null, 3, 5, null);
-        expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-        nota = new Nota(null, 7, 4, null);
-        expect(nota.mediaFinal()).toEqual(0.4 * 7 + 0.6 * 4);
-    });
-
-    test('a média é (0.4 * a3) + (0.6 * a2) se a3 substitui a1', () => {
-        let nota = new Nota(null, 0, 5, 3);
-        expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-        nota = new Nota(null, 2, 5, 3);
-        expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-        nota = new Nota(null, 2, 5, 6);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-    });
-
-    test('a média é (0.4 * a1) + (0.6 * a3) se a3 substitui a2', () => {
-        let nota = new Nota(null, 6, 0, 5);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-        nota = new Nota(null, 6, 4, 5);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-        nota = new Nota(null, 6, 4, 7);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 7);
-    });
+test('calcula conceito final corretamente', () => {
+    let notaObj = nota('disciplina_teste', 9.5, 9.5, 9.5);
+    expect(notaObj.mediaCA()).toBe('SS');
+    notaObj = nota('disciplina_teste', 8.0, 8.0, 8.0);
+    expect(notaObj.mediaCA()).toBe('MS');
+    notaObj = nota('disciplina_teste', 6.0, 6.0, 6.0);
+    expect(notaObj.mediaCA()).toBe('MM');
+    notaObj = nota('disciplina_teste', 4.0, 4.0, 4.0);
+    expect(notaObj.mediaCA()).toBe('MI');
+    notaObj = nota('disciplina_teste', 2.0, 2.0, 2.0);
+    expect(notaObj.mediaCA()).toBe('II');
+    notaObj = nota('disciplina_teste', 0.0, 0.0, 0.0);
+    expect(notaObj.mediaCA()).toBe('SR');
 });
