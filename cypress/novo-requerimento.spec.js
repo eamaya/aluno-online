4496febb677a2f53788026f62058f6ec81bed4a9
unknown <ymeir02@gmail.com>
2023-06-22 15:07:37 -0300

    Concluir implementação dos testes e2e

---
+++ cypress/novo-requerimento.spec.js
@@ -0,0 +1,18 @@
+describe('Testes E2E - Novo Requerimento', () => {
+    beforeEach(() => {
+      // Comandos a serem executados antes de cada teste
+    })
+  
+    it('Deve preencher e enviar o formulário de novo requerimento', () => {
+      // Comandos para preencher o formulário de novo requerimento
+      // e verificar se o envio foi realizado com sucesso
+    })
+  
+    it('Deve exibir uma mensagem de erro ao enviar um formulário inválido', () => {
+      // Comandos para preencher o formulário de novo requerimento com dados inválidos
+      // e verificar se uma mensagem de erro é exibida corretamente
+    })
+  
+    // Outros testes para validar diferentes cenários da funcionalidade
+  })
+  
